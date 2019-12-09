958cf175a4b05b46db27b3ba3f378703a3c0a1a3
Mohammad Matini <mohammad.matini@kayisoft.net>
2019-12-09 16:59:47 +0300

    Mark the `id` column as a PRIMARY KEY column

--- migrations.lisp
+++ migrations.lisp
@@ -16,4 +16,29 @@
      :date "2019-10-27T09:09:08Z"
      :description "Add a visit counter column"
      :up ("ALTER TABLE urls ADD visits INTEGER DEFAULT 0")
-     :down ("ALTER TABLE urls DROP COLUMN visit_count"))))
+     :down ("ALTER TABLE urls DROP COLUMN visit_count"))
+
+    (:id 3
+     :date "2019-12-09T16:58:40Z"
+     :description "Mark `id` as a PRIMARY KEY column. SQLite does not
+     fully support ALTER table, so we have to recreate the table see:
+     https://www.sqlite.org/lang_altertable.html for more information"
+     :up ("CREATE TABLE IF NOT EXISTS new_urls (
+           id VARCHAR(12) NOT NULL PRIMARY KEY,
+           url TEXT,
+           visits INTEGER DEFAULT 0,
+           created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)"
+
+          "INSERT INTO new_urls SELECT * FROM urls"
+          "DROP TABLE urls"
+          "ALTER TABLE new_urls RENAME TO urls")
+
+     :down ("CREATE TABLE IF NOT EXISTS new_urls (
+             id VARCHAR(12) UNIQUE,
+             url TEXT,
+             visits INTEGER DEFAULT 0,
+             created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)"
+
+            "INSERT INTO new_urls SELECT * FROM urls"
+            "DROP TABLE urls"
+            "ALTER TABLE new_urls RENAME TO urls"))))
